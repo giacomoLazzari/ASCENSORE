f6bc6629f3f6c00a928121857f32c2b1b1fddfa6
giacomoLazzari <giacomo.lazzari.stud@ispascalcomandini.it>
2021-02-16 16:42:53 +0100

    Primo metodo dello spostamento funzionante ma non si vede lo spostamento

--- ASCENSORE/MainWindow.xaml.cs
+++ ASCENSORE/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,95 @@ namespace ASCENSORE
     /// </summary>
     public partial class MainWindow : Window
     {
+        public readonly Uri uriAscensore = new Uri("ascensore.png", UriKind.Relative);
+        public Queue<int> listaPrenotazioni;
+        private static Semaphore _pool;
+        Thread t0;
+        Thread t1;
+        Thread t2;
+        Thread t3;
+        Thread t4;
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            ImageSource immAscensore = new BitmapImage(uriAscensore);
+            imgAscensore.Source = immAscensore;
+            _pool = new Semaphore(0, 1);
+            t1 = new Thread(new ThreadStart(MetodoBottone1));
+            _pool.Release(1);
+            
+        }
+
+        private void edificio1_Click(object sender, RoutedEventArgs e)
+        {
+
+            t1.Start();
+            t1.Join();
+            
+        }
+
+        public void MetodoBottone1()
+        {
+            _pool.WaitOne();
+            {
+                double g = 260.501;
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (imgAscensore.Margin.Top - g > 0)
+                    {
+                        double f = imgAscensore.Margin.Top;
+                        double i = imgAscensore.Margin.Bottom;
+                        while (f > 260)
+                        {
+                            f--;
+                            i++;
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                imgAscensore.Margin = new Thickness(178, f, 501.333, i);
+                            }));
+                            Thread.Sleep(40);
+                        }
+                    }
+                    else if (imgAscensore.Margin.Top - g < 0)
+                    {
+                        double f = imgAscensore.Margin.Top;
+                        double i = imgAscensore.Margin.Bottom;
+                        while (f < 260)
+                        {
+                            f++;
+                            i--;
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                imgAscensore.Margin = new Thickness(178, f, 501.333, i);
+                            }));
+                            Thread.Sleep(40);
+                        }
+                    }
+                }));
+            }
+            _pool.Release();
+        }
+
+        private void edificio0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void edificio2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void edificio3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void edificio4_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
